9c6f1712b6272ae428d4c67bf07434dacddbed2e
Lamadrid-Mike <mikealamadrid@hotamil.com>
2022-07-30 22:57:54 -0400

    Edit Resume Component

--- src/components/resume.js
+++ src/components/resume.js
@@ -7,28 +7,32 @@ import {
   faLocationDot,
   faCode,
 } from "@fortawesome/free-solid-svg-icons";
+import Footer from "./footer";
 
 class Resume extends React.Component {
   render() {
     return (
-      <div className="resume">
-        <div className="tittle">
-          <h2>Full Stack Web Developer</h2>
+      <>
+        <div className="resume">
+          <div className="tittle">
+            <h2>Full Stack Web Developer</h2>
+          </div>
+          <div className="contactInfo">
+            <h3>Mike Lamadrid</h3>
+            <p>
+              <FontAwesomeIcon className="icon" icon={faEnvelope} />
+              mikealamadrid@hotmail.com <br />
+              <FontAwesomeIcon className="icon" icon={faPhone} />
+              1-862-888-4791 <br />
+              <FontAwesomeIcon className="icon" icon={faLocationDot} />
+              Passaic, New Jersey <br />
+              <FontAwesomeIcon className="icon" icon={faCode} />
+              github.com/Lamadrid-Mike <br />
+            </p>
+          </div>
         </div>
-        <div className="contactInfo">
-          <h3>Mike Lamadrid</h3>
-          <p>
-            <FontAwesomeIcon className="icon" icon={faEnvelope} />
-            mikealamadrid@hotmail.com <br />
-            <FontAwesomeIcon className="icon" icon={faPhone} />
-            1-862-888-4791 <br />
-            <FontAwesomeIcon className="icon" icon={faLocationDot} />
-            Passaic, New Jersey <br />
-            <FontAwesomeIcon className="icon" icon={faCode} />
-            github.com/Lamadrid-Mike <br />
-          </p>
-        </div>
-      </div>
+        <Footer />
+      </>
     );
   }
 }
