ba4e6461bd8646846773a67b82ecac76d6eb0415
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-02-28 23:48:48 -0500

    css files edited

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,7 +5,8 @@ import { faEnvelope, faPhone, faUser } from "@fortawesome/free-solid-svg-icons";
 import Footer from "./footer";
 import BackArrow from "./BackArrow";
 import { Link } from "react-router-dom";
-import Logo from "./avatar.jpg";
+import Logo from "../img/profile.png";
+import Fade from "react-reveal/Fade";
 
 // page
 class Resume extends React.Component {
@@ -15,25 +16,28 @@ class Resume extends React.Component {
         <Link to="/">
           <BackArrow />
         </Link>
-        <div className="contact-container">
-          <img src={Logo} />
-          <div className="contact-info">
-            <p>
-              <FontAwesomeIcon className="icon" icon={faUser} />
-              Mike Lamadrid
-            </p>
-            <p>
-              <FontAwesomeIcon className="icon" icon={faEnvelope} />
-              <a href="mailto:mikealamadrid@hotmail.com?subject=Hello world!&body=">
-                mikealamadrid@hotmail.com
-              </a>
-            </p>
-            <p>
-              <FontAwesomeIcon className="icon" icon={faPhone} />
-              <a href="tel:862-888-4791">862-888-4791</a>
-            </p>
+        <h1 className="contact-h1">Contact me...</h1>
+        <Fade left>
+          <div className="contact-container">
+            <img src={Logo} />
+            <div className="contact-info">
+              <p>
+                <FontAwesomeIcon className="icon" icon={faUser} />
+                Mike Lamadrid
+              </p>
+              <p>
+                <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                <a href="mailto:mikealamadrid@hotmail.com?subject=Hello world!&body=">
+                  mikealamadrid@hotmail.com
+                </a>
+              </p>
+              <p>
+                <FontAwesomeIcon className="icon" icon={faPhone} />
+                <a href="tel:862-888-4791">862-888-4791</a>
+              </p>
+            </div>
           </div>
-        </div>
+        </Fade>
         <Footer />
       </>
     );
