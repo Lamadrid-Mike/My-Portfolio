497ee6b3a4a698cd1d5ea132d11bcceed9a82996
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-03-04 21:48:16 -0500

    spinner added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Projects.css";
 import BackArrow from "./BackArrow";
-import ReactLogo from "../img/react-logo.png";
-import JsLogo from "../img/JsLogo.png";
+import Loading from "../img/loading-spinner.png";
 
 const Projects = function (props) {
   return (
@@ -13,7 +12,7 @@ const Projects = function (props) {
       </Link>
       <div className="projects-container">
         <div className="projects-cards">
-          <img src={ReactLogo} className="first-project-logo logo-properties" />
+          <img src={Loading} className="first-project-logo logo-properties" />
           <h1 className="align-text">
             {/* Implemented React.js for this project, my main goal was to make it
             mobile responsive, and creation of the footer. */}
