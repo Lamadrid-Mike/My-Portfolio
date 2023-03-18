94bc5a3d30fb54bc3aebd758f34098fcf6cf950c
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-03-17 22:45:33 -0400

    files edited

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -5,6 +5,7 @@ import mySelf from "../img/profile.jpg";
 import BackArrow from "./BackArrow";
 import { Link } from "react-router-dom";
 import ProgressBar from "./ProgressBar";
+import { Slide } from "react-awesome-reveal";
 
 const About = function () {
   return (
@@ -13,33 +14,35 @@ const About = function () {
         <BackArrow />
       </Link>
       <div>
-        <h1 className="about-h1">
-          Hello
-          <br />
-          <span>A bit about me...</span>
-        </h1>
-        <div className="about-container">
-          <div className="about-profile">
-            <img alt="my-self" src={mySelf} />
-            <h4>Web Developer</h4>
-          </div>
-          <div className="about-text">
-            Coding has always caught my eye, so I made the decision to enroll in
-            a bootcamp. I haven't looked back since, love learning new
-            programming skills every day.
-          </div>
-          <div className="skill-parent-container">
-            <h3>SKILLS</h3>
-            <div className="skill-container">
-              <ProgressBar type="CSS" percentage={90} />
-              <ProgressBar type="HTML" percentage={80} />
+        <Slide cascade triggerOnce>
+          <h1 className="about-h1">
+            Hello
+            <br />
+            <span>A bit about me...</span>
+          </h1>
+          <div className="about-container">
+            <div className="about-profile">
+              <img alt="my-self" src={mySelf} />
+              <h4>Web Developer</h4>
             </div>
-            <div className="skill-container">
-              <ProgressBar type="Java Script" percentage={80} />
-              <ProgressBar type="React.js" percentage={70} />
+            <div className="about-text">
+              Coding has always caught my eye, so I made the decision to enroll
+              in a bootcamp. I haven't looked back since, love learning new
+              programming skills every day.
+            </div>
+            <div className="skill-parent-container">
+              <h3>SKILLS</h3>
+              <div className="skill-container">
+                <ProgressBar type="CSS" percentage={90} />
+                <ProgressBar type="HTML" percentage={80} />
+              </div>
+              <div className="skill-container">
+                <ProgressBar type="Java Script" percentage={80} />
+                <ProgressBar type="React.js" percentage={70} />
+              </div>
             </div>
           </div>
-        </div>
+        </Slide>
       </div>
       <Footer />
     </>
--- src/components/resume.js
+++ src/components/resume.js
@@ -11,8 +11,8 @@ import Footer from "./footer";
 import BackArrow from "./BackArrow";
 import { Link } from "react-router-dom";
 import avatar from "../img/profile.jpg";
+import { Slide } from "react-awesome-reveal";
 
-// page
 class Resume extends React.Component {
   constructor(props) {
     super(props);
@@ -40,31 +40,33 @@ class Resume extends React.Component {
           <BackArrow />
         </Link>
         <h1 className="contact-h1">Contact me...</h1>
-        <div className="contact-container">
-          <img alt="logo" src={avatar} />
-          <div className="contact-info">
-            <p>
-              <FontAwesomeIcon className="icon" icon={faUser} />
-              Mike Lamadrid
-            </p>
-            <p>
-              <FontAwesomeIcon className="icon" icon={faEnvelope} />
-              <a href="mailto:mikealamadrid@hotmail.com?subject=Hello world!&body=">
-                mikealamadrid@hotmail.com
-              </a>
-            </p>
-            <p>
-              <FontAwesomeIcon className="icon" icon={faPhone} />
-              <a href="tel:862-888-4791">862-888-4791</a>
-            </p>
+        <Slide>
+          <div className="contact-container">
+            <img alt="logo" src={avatar} />
+            <div className="contact-info">
+              <p>
+                <FontAwesomeIcon className="icon" icon={faUser} />
+                Mike Lamadrid
+              </p>
+              <p>
+                <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                <a href="mailto:mikealamadrid@hotmail.com?subject=Hello world!&body=">
+                  mikealamadrid@hotmail.com
+                </a>
+              </p>
+              <p>
+                <FontAwesomeIcon className="icon" icon={faPhone} />
+                <a href="tel:862-888-4791">862-888-4791</a>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="btn-container">
-          <button onClick={this.resumeBtn} className="resume-btn">
-            <FontAwesomeIcon className="icon" icon={faDownload} />
-            My Resume
-          </button>
-        </div>
+          <div className="btn-container">
+            <button onClick={this.resumeBtn} className="resume-btn">
+              <FontAwesomeIcon className="icon" icon={faDownload} />
+              My Resume
+            </button>
+          </div>
+        </Slide>
         <Footer />
       </div>
     );
