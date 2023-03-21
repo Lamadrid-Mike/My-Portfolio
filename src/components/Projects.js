86f10397f3650009295f95a9a0596afb3cbbe815
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-03-21 19:49:10 -0400

    new project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom";
 import "./Projects.css";
 import BackArrow from "./BackArrow";
 import Footer from "./footer";
-import jsLogo from "../img/JsLogo.png";
-import reactLogo from "../img/react-logo.png";
 import Cards from "./Cards";
 import { motion } from "framer-motion";
+import jsLogo from "../img/JsLogo.png";
+import reactLogo from "../img/react-logo.png";
+import todoLogo from "../img/todo-logo.png";
 
 const Projects = function () {
   return (
@@ -35,6 +36,17 @@ const Projects = function () {
             link="https://nexustential.github.io/MiraclesMassage"
             src={reactLogo}
             info="Miracles-Massage"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ x: "-100%" }}
+          animate={{ x: "0%" }}
+          transition={{ duration: 0.8 }}
+        >
+          <Cards
+            link="https://oop-todo-app.netlify.app/"
+            src={todoLogo}
+            info="OOP Todo app"
           />
         </motion.div>
       </div>
