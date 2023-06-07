640c4251ba7b1095d3041eea50a49111f7d20303
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-06-07 19:29:42 -0400

    files edited

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/My-Portfolio-React">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
