e99c9c6ac40ca524512f9278732bee8679c1adc2
Lamadrid-Mike <mikealamadrid@hotmail.com>
2024-11-30 15:41:50 -0500

    files added

--- src/components/MainSlider.js
+++ src/components/MainSlider.js
@@ -1,5 +1,5 @@
 import "../styles/MainSlider.scss";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import leftBtn from "../icons/left-btn.svg";
 import rightBtn from "../icons/right-btn.svg";
 
@@ -24,18 +24,29 @@ const images = [
 
 export default function MainSlider() {
   const [currIndex, setCurrIndex] = useState(0);
+  const interval = useRef(null);
+
+  useEffect(() => {
+    interval.current = setInterval(() => {
+      nextSlide();
+    }, 4000);
+
+    return () => clearInterval(interval.current);
+  }, []);
 
   function prevSlide() {
     setCurrIndex((index) => (index - 1 + images.length) % images.length);
+    clearInterval(interval.current);
   }
 
   function nextSlide() {
     setCurrIndex((index) => (index + 1) % images.length);
+    clearInterval(interval.current);
   }
 
   return (
     <div className="slider__container">
-      <button className="slider__btn-prev" onClick={prevSlide}>
+      <button className="slider__btn-prev slider__btn" onClick={prevSlide}>
         <img src={leftBtn} alt="left-btn"></img>
       </button>
       {images.map((img, index) => (
@@ -63,10 +74,11 @@ export default function MainSlider() {
             >
               {img.description}
             </p>
+            <button className="learn-more__btn">Learn More</button>
           </div>
         </div>
       ))}
-      <button className="slider__btn-next" onClick={nextSlide}>
+      <button className="slider__btn-next slider__btn" onClick={nextSlide}>
         <img src={rightBtn} alt="right-btn"></img>
       </button>
     </div>
